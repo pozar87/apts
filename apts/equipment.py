6bb6e56f54a60d66992e1c58b8d9947d1a3c4990
Łukasz Pożarlik <lpozarlik@gmail.com>
2017-09-13 22:04:52 +0200

    Better handling for allowed optical paths
    * All optical equipment is now automatically added with proper input and
    outpust nodes
    * Add connect() function that connect all outputs node with relevant
    inputs
    * Graph node store optical equipment
    * Add labels to graph
    * Add coloring to node

--- apts/equipment.py
+++ apts/equipment.py
@@ -1,7 +1,15 @@
 import igraph
 from . import utils
 from .models.optical import *
+from enum import Enum
 
+
+class Type(Enum):
+  OPTICAL = 1
+  INPUT = 2
+  OUTPUT = 3
+  IMAGE = 4
+  
 
 class Equipment(object):
   """This class represents all possesed astronomical equpiment. Allows to computer
@@ -11,51 +19,95 @@ class Equipment(object):
   SPACE_ID = "space"
   IMAGE_ID = "image"
   
+  OUT = "_out"
+  IN = "_in"
+  
+  COLORS = {
+      Type.OPTICAL : "blue",
+      Type.INPUT : "red",
+      Type.OUTPUT : "green",
+      Type.IMAGE : "yellow"
+  } 
+  
+  TYPE = "node_type"
+  LABEL = "label"
+  COLOR = "color"
+  EQUIPMENT = "equipment" 
+  
   def __init__(self):
-    self.optical_items = {}
     self.connection_garph = igraph.Graph(directed=True) 
-    self.connection_garph.add_vertex(Equipment.SPACE_ID)
-    self.connection_garph.add_vertex(Equipment.IMAGE_ID)  
+    self.add_vertex(Equipment.SPACE_ID, Equipment.SPACE_ID, Type.IMAGE)
+    self.add_vertex(Equipment.IMAGE_ID, Equipment.IMAGE_ID, Type.IMAGE)
   
   def get_possiable_views(self):
-    for optical_path in utils.find_all_paths(self.connection_garph,
-        self.connection_garph.vs.find(name=Equipment.SPACE_ID).index,
-        self.connection_garph.vs.find(name=Equipment.IMAGE_ID).index):
-  
-      print([str(self.optical_items[id]) for id in optical_path if id in self.optical_items.keys()])
+    # connect all outputs with inputs
+    self.connect()
+    space_node = self.connection_garph.vs.find(name=Equipment.SPACE_ID)
+    image_node = self.connection_garph.vs.find(name=Equipment.IMAGE_ID)
+    for optical_path in utils.find_all_paths(self.connection_garph, space_node.index, image_node.index):
+      result = [self.connection_garph.vs.find(name=id)[Equipment.EQUIPMENT] for id in optical_path]
+      print([str(item) for item in result if item is not None])
+      
   def plot_connection_garph(self):
     return igraph.plot(self.connection_garph)
   
   def get_all(self):
     return self.optical_items
      
-  def register(self, item, optical):
-    try: #TODO: this is realy ugly ... :(
-      self.connection_garph.vs.find(name=str(item))
-    except ValueError:  
-      self.connection_garph.add_vertex(str(item)) 
-      id = self.connection_garph.vs.find(name=str(item)).index #TODO: fix unique names
-    if optical:
-      self.optical_items[id] = item   
+  def connect(self):
+    for out_node in self.connection_garph.vs.select(node_type = Type.OUTPUT):
+      for in_node in self.connection_garph.vs.select(node_type = Type.INPUT):
+        #connect all outputs with all inputs
+        self.add_edge(out_node, in_node)
 
-  def register_telescope(self, new_telescope):    
-    self.register(new_telescope, True) 
-    self.register(new_telescope.output_size, False)  
-        
-    self.connection_garph.add_edge(Equipment.SPACE_ID,str(new_telescope))
-    self.connection_garph.add_edge(str(new_telescope),new_telescope.output_size)
+     
+  def add_vertex(self, node_name, node_label, node_type, equipment = None):
+    """ 
+    Add single node to graph. Return vertex
+    """
+    self.connection_garph.add_vertex(node_name) 
+    node = self.connection_garph.vs.find(name = node_name)
+    node[Equipment.LABEL] = node_label
+    node[Equipment.TYPE] = node_type
+    node[Equipment.COLOR] = Equipment.COLORS[node_type]
+    node[Equipment.EQUIPMENT] = equipment
+    return node  
+  
+  def add_edge(self, node_from, node_to):
+    self.connection_garph.add_edge(node_from, node_to)
+
+  def register_telescope(self, new_telescope):
+    """ 
+    Telescope node is build out of multipe verteces - telescope and its outputs.  
+    """    
+    #add telescope vertex
+    self.add_vertex(new_telescope.id, new_telescope.label(), Type.OPTICAL, new_telescope) 
+    #add telescope output vertex
+    self.add_vertex(new_telescope.id + Equipment.OUT, new_telescope.label() + Equipment.OUT, Type.OUTPUT) 
+    #connect telescope vertex to space
+    self.add_edge(Equipment.SPACE_ID, new_telescope.id)
+    #connect telescope vertex to its outpus
+    self.add_edge(new_telescope.id, new_telescope.id + Equipment.OUT)
     
   def register_ocular(self, new_okular):    
-    self.register(new_okular, True)
-    self.register(new_okular.size, False)  
- 
-    self.connection_garph.add_edge(str(new_okular),Equipment.IMAGE_ID)
-    self.connection_garph.add_edge(new_okular.size,str(new_okular))
-    
-  def register_barlow(self, new_barlow, for_telescope):    
-    self.register(new_barlow, True)
-    self.register(new_barlow.size, False)  
- 
-    self.connection_garph.add_edge(str(for_telescope), str(new_barlow))  
-    self.connection_garph.add_edge(str(new_barlow), new_barlow.size)  
+    #add ocular vertex
+    self.add_vertex(new_okular.id, new_okular.label(), Type.OPTICAL, new_okular)
+    #add ocular input vertex
+    self.add_vertex(new_okular.id + Equipment.IN, new_okular.label() + Equipment.IN, Type.INPUT) 
+    #connect ocular with output image vertex 
+    self.add_edge(new_okular.id, Equipment.IMAGE_ID)
+    #connect input to ocular vertex 
+    self.add_edge(new_okular.id + Equipment.IN, new_okular.id)
+  
+  def register_barlow(self, new_barlow):   
+    #add barlow vertex
+    self.add_vertex(new_barlow.id, new_barlow.label(), Type.OPTICAL, new_barlow)
+    #add barlow input vertex
+    self.add_vertex(new_barlow.id + Equipment.IN, new_barlow.label() + Equipment.IN, Type.INPUT) 
+    #add barlow output vertex
+    self.add_vertex(new_barlow.id + Equipment.OUT, new_barlow.label() + Equipment.OUT, Type.OUTPUT) 
+    #connect barlow with output vertex 
+    self.add_edge(new_barlow.id, new_barlow.id + Equipment.OUT)
+    #connect input with barlow vertex
+    self.add_edge(new_barlow.id + Equipment.IN, new_barlow.id)
     
--- apts/models/optical.py
+++ apts/models/optical.py
@@ -1,13 +1,18 @@
+import uuid
+
 class OpticalEqipment(object):
   """Basic class for optical equipment"""
   
   def __init__(self, focal_length, vendor):
     self.focal_length = focal_length
     self.vendor = vendor 
+    self.id = str(uuid.uuid4())
 
   def __str__(self):
     return "{}, f={}".format(self.vendor,self.focal_length)
 
+  def label(self):
+    return str(self)
 
 class Telescope(OpticalEqipment):
   """Class representing telescope"""
@@ -20,17 +25,19 @@ class Telescope(OpticalEqipment):
 class Ocular(OpticalEqipment):   
   """Class representing ocular"""
   
-  def __init__(self, focal_length,vendor = "unknown ocular", size = "1.25"):
-    self.size = size
+  def __init__(self, focal_length,vendor = "unknown ocular", input_size = "1.25"):
+    self.input_size = input_size
     super(Ocular, self).__init__(focal_length, vendor)
     
 class Barlow(OpticalEqipment):   
   """Class representing barlow lense"""
   
-  def __init__(self, focal_length,vendor = "unknown barlow", size = "1.25"):
-    self.size = size
-    super(Barlow, self).__init__(focal_length, vendor)
+  def __init__(self, magnification,vendor = "unknown barlow", input_size = "1.25", output_size = "1.25"):
+    self.input_size = input_size
+    self.output_size = output_size
+    self.magnification = magnification
+    super(Barlow, self).__init__(0, vendor)
     
   def __str__(self):
-    return "{}, x{}".format(self.vendor,self.focal_length)
+    return "{}, x{}".format(self.vendor,self.magnification)
 
