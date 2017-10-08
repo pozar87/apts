a5ae120c8df5cf1f45ca92137b98236bbc58b831
Łukasz Pożarlik <lpozarlik@gmail.com>
2017-10-08 10:14:51 +0200

    Time zone support added
    * Observation and Place are now aware of timezone
    * Add ozone chart

--- apts/observations.py
+++ apts/observations.py
@@ -1,4 +1,9 @@
 import ephem
+import pytz
+from dateutil import tz
+
+
+from .utils import ureg
 from .weather import Weather 
 
 class Place(ephem.Observer):
@@ -10,12 +15,13 @@ class Place(ephem.Observer):
     self.sun = ephem.Sun()
     self.sun.compute(self)
     self.weather = Weather(lat,lon)
+    self.local_timezone = tz.gettz(self.weather.local_timezone)
  
   def sunset(self):
-    return self.next_setting(self.sun)
+    return self.next_setting(self.sun).datetime().replace(tzinfo=pytz.UTC).astimezone(self.local_timezone)
   
   def sunrise(self):
-    return self.next_rising(self.sun)
+    return self.next_rising(self.sun).datetime().replace(tzinfo=pytz.UTC).astimezone(self.local_timezone)
     
   def sun_observation(self): 
     prevrise = self.next_rising(self.sun)
@@ -24,30 +30,40 @@ class Place(ephem.Observer):
     return (str(prevrise), str(transit), str(nextset))
    
 class Observation:
+
+  MAX_CLOUDS_TRESHOLD = 0.2
+  MAX_WIND_TRESHOLD = 5
+  MIN_TEMP_TRESHOLD = 5
+  
+
   def __init__(self, place, equipment):
     self.place = place
     self.equipment = equipment
-    self.observation_start = 24*(self.place.sunset() - ephem.now())
-    self.observation_stop = 24*(self.place.sunrise() - ephem.now())
+    self.observation_start = self.place.sunset()
+    self.observation_stop = self.place.sunrise()
+  
+  def _mark_observation(self,plot):
+    plot.axvspan(self.observation_start, self.observation_stop, color='gray', alpha=0.2)
   
   def plot_weather(self):
-    import matplotlib.pyplot as plt
-    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(20, 10))
+    import matplotlib.pyplot as plt #TODO: move it form here
+    fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(15, 10))
     # Clouds
     plt = self.place.weather.plot_clouds(ax=axes[0,0])
-    axes[0,0].set_title('Clouds');
-    plt.axvspan(self.observation_start, self.observation_stop, color='green', alpha=0.2)
-    plt.axhspan(0, 0.2, color='green', alpha=0.2) 
+    plt.axhspan(0, Observation.MAX_CLOUDS_TRESHOLD, color='green', alpha=0.2)
+    self._mark_observation(plt) 
     # Temperature
     plt = self.place.weather.plot_temperature(ax=axes[0,1])
-    axes[0,1].set_title('Temperature');
+    plt.axhspan(Observation.MIN_TEMP_TRESHOLD, 12.5, color='green', alpha=0.2) 
+    self._mark_observation(plt)
     # Wind
     plt = self.place.weather.plot_wind(ax=axes[1,0])
-    axes[1,0].set_title('Wind');
+    plt.axhspan(0, Observation.MAX_WIND_TRESHOLD, color='green', alpha=0.2)
+    self._mark_observation(plt)
     # Pressure
-    plt = self.place.weather.plot_pressure(ax=axes[1,1])
-    axes[1,1].set_title('Pressure');
-    
+    plt = self.place.weather.plot_pressure_and_ozone(ax=axes[1,1])
+    self._mark_observation(plt)
+    fig.tight_layout()
  
     
     
--- apts/weather.py
+++ apts/weather.py
@@ -1,7 +1,6 @@
 import urllib.request, json
 import pandas as pd
 
-
 class Weather:
 
   API_KEY = ""
@@ -11,21 +10,23 @@ class Weather:
     self.lat = lat
     self.lon = lon
     self.data = self.get_data()
+    self.local_timezone = ""
      
   def plot_clouds(self, **args):
-    plt = self.data[["cloudCover"]].plot(ylim=(0, 1), **args)
+    plt = self.data.plot(x = "time", y = "cloudCover", ylim=(0, 1.05), title="Clouds", **args)
     return plt 
   
   def plot_temperature(self, **args):
-    plt = self.data[["temperature","dewPoint","apparentTemperature"]].plot(**args)
+    plt = self.data[["time","temperature","apparentTemperature","dewPoint"]].plot(x = "time", title="Temperatures", **args)
     return plt 
     
   def plot_wind(self, **args):
-    plt = self.data[["windSpeed"]].plot(**args)
+    max_wind_speed = self.data[["windSpeed"]].max().max()
+    plt = self.data.plot(x = "time", y = "windSpeed", ylim=(0, max_wind_speed  + 1), title="Wind speed", **args)
     return plt    
   
-  def plot_pressure(self, **args):
-    plt = self.data[["pressure"]].plot(**args)
+  def plot_pressure_and_ozone(self, **args):
+    plt = self.data[["time","pressure","ozone"]].plot(x = "time", title="Pressure and Ozon", secondary_y=['ozone'],  **args)
     return plt  
   
   def get_data(self):  
@@ -46,5 +47,8 @@ class Weather:
                  "pressure",
                  "ozone"]
       json_data = json.loads(url.read().decode())
+      self.local_timezone = json_data["timezone"]
       raw_data = [[item[column] for column in columns] for item in json_data["hourly"]["data"]]
-      return pd.DataFrame(raw_data, columns=columns)
+      result = pd.DataFrame(raw_data, columns=columns)
+      result.time = result.time.astype('datetime64[s]').dt.tz_localize('UTC').dt.tz_convert(self.local_timezone)
+      return result
