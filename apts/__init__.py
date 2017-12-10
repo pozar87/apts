46a66c77617fe63e4c027e6512d8e8ed440a0faa
Łukasz Pożarlik <lpozarlik@gmail.com>
2017-12-10 12:41:09 +0100

    Update version to 0.2.3

--- apts/__init__.py
+++ apts/__init__.py
@@ -20,4 +20,4 @@ Weather.API_URL = config['weather']['api_url']
 Notify.EMAIL_ADDRESS = config['notification']['email_address']
 Notify.EMAIL_PASSWORD = config['notification']['email_password']
 
-__version__ = '0.2.1'
+__version__ = '0.2.3'
