7a6f2344c5121f8fa108a4ce58a09db49cb8f7e3
Łukasz Pożarlik <lpozarlik@gmail.com>
2018-01-13 09:00:53 +0100

    Refactor
    * Extracted Place class
    * Better names of methods in Place class
    * Fix hardcoded elevation

---
+++ apts/place.py
@@ -0,0 +1,42 @@
+import ephem
+import pytz
+
+from dateutil import tz
+
+from .weather import Weather
+
+class Place(ephem.Observer):
+  def __init__(self, lat, lon, name="", elevation = 300, *args):
+    ephem.Observer.__init__(self, *args)
+    self.lat = str(lat)
+    self.lon = str(lon)
+    self.name = name
+    self.elevation = elevation
+    # Sun
+    self.sun = ephem.Sun()
+    self.sun.compute(self)
+    # Moon
+    self.moon = ephem.Moon()
+    self.moon.compute(self)
+
+    self.weather = Weather(lat, lon)
+    self.local_timezone = tz.gettz(self.weather.local_timezone)
+
+  def _next_setting_time(self, obj):
+    return self.next_setting(obj).datetime().replace(tzinfo=pytz.UTC).astimezone(self.local_timezone)
+
+  def _next_rising_time(self, obj):
+    return self.next_rising(obj).datetime().replace(tzinfo=pytz.UTC).astimezone(self.local_timezone)
+
+  def sunset_time(self):
+    return self._next_setting_time(self.sun)
+
+  def sunrise_time(self):
+    return self._next_rising_time(self.sun)
+
+  def moonset_time(self):
+    return self._next_setting_time(self.moon)
+
+  def moonrise_time(self):
+    return self._next_rising_time(self.moon)
+
