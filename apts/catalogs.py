34c87e3320ba0986e5ad6682086270e94aaa1a43
Łukasz Pożarlik <lpozarlik@gmail.com>
2018-02-17 22:41:39 +0100

    Sorted requirements.txt

--- apts/catalogs.py
+++ apts/catalogs.py
@@ -4,6 +4,6 @@ import pkg_resources
 class Catalogs:
   # Load Messier catalogue data
   MESSIER = pandas.read_csv(pkg_resources.resource_filename('apts', 'data/messier.csv'))
-  # Load NGC
+  # Load NGC catalogue data
   #NGC = pandas.read_csv(pkg_resources.resource_filename('apts', 'data/ngc.csv'))
 
