e145f4e2bc5e673c540a0d13fa9ca02629bd371b
Łukasz Pożarlik <lpozarlik@gmail.com>
2025-04-16 17:05:03 +0200

    Standardize quotes and bump version to 0.4.2

--- apts/__init__.py
+++ apts/__init__.py
@@ -13,7 +13,7 @@ from .place import Place
 from .utils import Utils
 from .weather import Weather
 
-__all__ = ['Catalogs', 'Equipment', 'Observation', 'Place', 'Utils']
+__all__ = ["Catalogs", "Equipment", "Observation", "Place", "Utils"]
 
 logger = logging.getLogger(__name__)
 
@@ -27,20 +27,28 @@ candidates = [example_config, user_config]
 config.read(candidates)
 
 # Set logging level from config
-log_level = config.get('logging', 'level', fallback='DEBUG')
+log_level = config.get("logging", "level", fallback="DEBUG")
 try:
     logger.setLevel(getattr(logging, log_level.upper()))
 except AttributeError:
     logger.setLevel(logging.DEBUG)
     logger.warning(f"Invalid logging level '{log_level}' in config. Using DEBUG level.")
 # Load static fields from config
-setattr(Weather, 'API_KEY', config.get('weather', 'api_key', fallback=""))
-setattr(Notify, 'EMAIL_ADDRESS', config.get('notification', 'email_address', fallback=""))
-setattr(Notify, 'EMAIL_PASSWORD', config.get('notification', 'email_password', fallback=""))
+setattr(Weather, "API_KEY", config.get("weather", "api_key", fallback=""))
+setattr(
+    Notify, "EMAIL_ADDRESS", config.get("notification", "email_address", fallback="")
+)
+setattr(
+    Notify, "EMAIL_PASSWORD", config.get("notification", "email_password", fallback="")
+)
 
 # Seaborn style
-sns.set_style('whitegrid' if config.get('style', 'seaborn', fallback='whitegrid') == 'whitegrid' else 'white')
+sns.set_style(
+    "whitegrid"
+    if config.get("style", "seaborn", fallback="whitegrid") == "whitegrid"
+    else "white"
+)
 # Disable label trimming in pandas tables
-pd.set_option('display.max_colwidth', None)
+pd.set_option("display.max_colwidth", None)
 
-__version__ = '0.4.1'
+__version__ = "0.4.2"
