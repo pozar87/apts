6e78094e7f01f9a236f27a28e25e0e48541fc623
Łukasz Pożarlik <lpozarlik@gmail.com>
2025-10-28 22:33:46 +0100

    Modify NASA API to handle large date ranges with pagination

--- apts/nasa_api.py
+++ apts/nasa_api.py
@@ -1,5 +1,5 @@
 import requests
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class NasaAPI:
@@ -8,11 +8,34 @@ class NasaAPI:
         self.base_url = "https://api.nasa.gov/neo/rest/v1/feed"
 
     def get_comets(self, start_date: datetime, end_date: datetime):
-        params = {
-            "start_date": start_date.strftime("%Y-%m-%d"),
-            "end_date": end_date.strftime("%Y-%m-%d"),
-            "api_key": self.api_key,
-        }
-        response = requests.get(self.base_url, params=params)
-        response.raise_for_status()
-        return response.json()
+        all_data = {}
+        current_start = start_date
+        while current_start <= end_date:
+            current_end = min(current_start + timedelta(days=6), end_date)
+            params = {
+                "start_date": current_start.strftime("%Y-%m-%d"),
+                "end_date": current_end.strftime("%Y-%m-%d"),
+                "api_key": self.api_key,
+            }
+            response = requests.get(self.base_url, params=params)
+            response.raise_for_status()
+            data = response.json()
+
+            if not all_data:
+                all_data = data
+            else:
+                # Merge the 'near_earth_objects'
+                for date_str, objects in data.get("near_earth_objects", {}).items():
+                    if date_str in all_data["near_earth_objects"]:
+                        all_data["near_earth_objects"][date_str].extend(objects)
+                    else:
+                        all_data["near_earth_objects"][date_str] = objects
+
+            current_start += timedelta(days=7)
+
+        # Update element_count
+        if "near_earth_objects" in all_data:
+            total_elements = sum(len(v) for v in all_data["near_earth_objects"].values())
+            all_data["element_count"] = total_elements
+
+        return all_data
