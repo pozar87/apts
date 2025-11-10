001b1f26b422367c0904f5b38366af12843db3b5
Łukasz Pożarlik <lpozarlik@gmail.com>
2025-11-10 23:18:55 +0100

    Add i18n support for y-axis labels and Polish "Time" translation
    - Wrap y_label with gettext_() to enable translation - Add Polish
    translation for "Time" in locale file - Reformat method signature for
    better readability

--- apts/utils/plot.py
+++ apts/utils/plot.py
@@ -1,14 +1,21 @@
-from matplotlib import pyplot
 import matplotlib.dates as mdates
+from matplotlib import pyplot
 
-from apts.i18n import gettext_
 from apts.constants.graphconstants import get_plot_style
+from apts.i18n import gettext_
+
 
 class Utils:
     @staticmethod
-    def annotate_plot(ax: pyplot.Axes, y_label: str, dark_mode: bool, local_timezone: object, x_label: str = "Time"):
+    def annotate_plot(
+        ax: pyplot.Axes,
+        y_label: str,
+        dark_mode: bool,
+        local_timezone: object,
+        x_label: str = "Time",
+    ):
         style = get_plot_style(dark_mode)
-        ax.set_ylabel(y_label, color=style["TEXT_COLOR"])
+        ax.set_ylabel(gettext_(y_label), color=style["TEXT_COLOR"])
         ax.set_xlabel(gettext_(x_label), color=style["TEXT_COLOR"])
         ax.tick_params(axis="x", colors=style["TICK_COLOR"])
         ax.tick_params(axis="y", colors=style["TICK_COLOR"])
