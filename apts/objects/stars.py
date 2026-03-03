16010d0c6a75bd9307e04bb29f7b1a33e05eb5c4
Łukasz Pożarlik <lpozarlik@gmail.com>
2026-03-03 14:45:47 +0100

    Refactor get_skyfield_object to reconstruct fixed bodies
    Improve robustness of skyfield object retrieval by adding logic to
    reconstruct fixed bodies from RA/DEC coordinates when the
    `skyfield_object` attribute is missing, particularly useful after
    unpickling objects.

--- apts/objects/stars.py
+++ apts/objects/stars.py
@@ -56,7 +56,14 @@ class Stars(Objects):
         return computed_df
 
     def get_skyfield_object(self, obj):
-        return obj.skyfield_object
+        if "skyfield_object" in obj:
+            return obj.skyfield_object
+
+        # Reconstruct if possible
+        if "ra_hours" in obj and "dec_degrees" in obj:
+            return self.fixed_body(obj["ra_hours"], obj["dec_degrees"])
+
+        return None
 
     def find_by_name(self, name):
         """
