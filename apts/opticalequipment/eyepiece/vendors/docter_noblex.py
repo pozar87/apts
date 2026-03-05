f565fbd0c10c9948557e298407cfe282e5042b30
Łukasz Pożarlik <lpozarlik@gmail.com>
2026-03-05 13:48:39 +0100

    Update eyepiece data to include more models
    This commit adds a comprehensive set of eyepieces from various
    manufacturers, including Agena, Antares, Astro-Physics, Astro-Tech,
    Baader, Brandon, Bresser, Docter/Noblex, Fujiyama, Generic, GSO, iStar,
    Kasai, Kokusai Kohki, Lacerta, Leica, Lunt, Lunt Solar, Masuyama,
    Maxvision, National Geographic, Omegon, Orion, Saxon, Siebert,
    Sky-Watcher, Stellarvue, Svbony, Tecnosky, TMB, TS-Optics, TPO, William
    Optics, and Zeiss.

--- apts/opticalequipment/eyepiece/vendors/docter_noblex.py
+++ apts/opticalequipment/eyepiece/vendors/docter_noblex.py
@@ -1,7 +1,45 @@
 from ..base import Eyepiece
 
 class Docter_noblexEyepiece(Eyepiece):
-    _DATABASE = {'Docter_Noblex_UWF_10mm': {'brand': 'Docter/Noblex', 'name': 'UWF 10mm', 'type': 'type_eyepiece', 'optical_length': 0, 'mass': 200, 'tside_thread': '1.25"', 'tside_gender': 'Male', 'cside_thread': '', 'cside_gender': '', 'reversible': False, 'bf_role': ''}, 'Docter_Noblex_UWF_12_5mm': {'brand': 'Docter/Noblex', 'name': 'UWF 12.5mm', 'type': 'type_eyepiece', 'optical_length': 0, 'mass': 220, 'tside_thread': '1.25"', 'tside_gender': 'Male', 'cside_thread': '', 'cside_gender': '', 'reversible': False, 'bf_role': ''}, 'Docter_Noblex_UWF_17mm': {'brand': 'Docter/Noblex', 'name': 'UWF 17mm', 'type': 'type_eyepiece', 'optical_length': 0, 'mass': 270, 'tside_thread': '1.25"', 'tside_gender': 'Male', 'cside_thread': '', 'cside_gender': '', 'reversible': False, 'bf_role': ''}}
+    _DATABASE = {       'Docter_Noblex_UWF_10mm': {       'bf_role': '',
+                                          'brand': 'Docter/Noblex',
+                                          'cside_gender': '',
+                                          'cside_thread': '',
+                                          'field_of_view_deg': 50,
+                                          'focal_length_mm': 10.0,
+                                          'mass': 200,
+                                          'name': 'UWF 10mm',
+                                          'optical_length': 0,
+                                          'reversible': False,
+                                          'tside_gender': 'Male',
+                                          'tside_thread': '1.25"',
+                                          'type': 'type_eyepiece'},
+        'Docter_Noblex_UWF_12_5mm': {       'bf_role': '',
+                                            'brand': 'Docter/Noblex',
+                                            'cside_gender': '',
+                                            'cside_thread': '',
+                                            'field_of_view_deg': 50,
+                                            'focal_length_mm': 12.5,
+                                            'mass': 220,
+                                            'name': 'UWF 12.5mm',
+                                            'optical_length': 0,
+                                            'reversible': False,
+                                            'tside_gender': 'Male',
+                                            'tside_thread': '1.25"',
+                                            'type': 'type_eyepiece'},
+        'Docter_Noblex_UWF_17mm': {       'bf_role': '',
+                                          'brand': 'Docter/Noblex',
+                                          'cside_gender': '',
+                                          'cside_thread': '',
+                                          'field_of_view_deg': 50,
+                                          'focal_length_mm': 17.0,
+                                          'mass': 270,
+                                          'name': 'UWF 17mm',
+                                          'optical_length': 0,
+                                          'reversible': False,
+                                          'tside_gender': 'Male',
+                                          'tside_thread': '1.25"',
+                                          'type': 'type_eyepiece'}}
 
     @classmethod
     def Docter_Noblex_UWF_10mm(cls):
