137dbf0c7c973c11cb34ed6b249aada862206683
Łukasz Pożarlik <lpozarlik@gmail.com>
2026-03-05 16:16:50 +0100

    Add sensor and image dimensions to Samsung cameras
    Includes sensor width/height, image width/height, and pixel size for
    NX1, NX500, NX300, and NX3000 cameras.

--- apts/opticalequipment/camera/vendors/samsung.py
+++ apts/opticalequipment/camera/vendors/samsung.py
@@ -1,7 +1,80 @@
 from ..base import Camera
 
 class SamsungCamera(Camera):
-    _DATABASE = {'Samsung_NX1': {'brand': 'Samsung', 'name': 'NX1', 'type': 'type_dslr', 'optical_length': 25.5, 'mass': 550, 'tside_thread': 'M42', 'tside_gender': 'Female', 'cside_thread': '', 'cside_gender': '', 'reversible': False, 'bf_role': 'end'}, 'Samsung_NX500': {'brand': 'Samsung', 'name': 'NX500', 'type': 'type_dslr', 'optical_length': 25.5, 'mass': 292, 'tside_thread': 'M42', 'tside_gender': 'Female', 'cside_thread': '', 'cside_gender': '', 'reversible': False, 'bf_role': 'end'}, 'Samsung_NX300': {'brand': 'Samsung', 'name': 'NX300', 'type': 'type_dslr', 'optical_length': 25.5, 'mass': 284, 'tside_thread': 'M42', 'tside_gender': 'Female', 'cside_thread': '', 'cside_gender': '', 'reversible': False, 'bf_role': 'end'}, 'Samsung_NX3000': {'brand': 'Samsung', 'name': 'NX3000', 'type': 'type_dslr', 'optical_length': 25.5, 'mass': 250, 'tside_thread': 'M42', 'tside_gender': 'Female', 'cside_thread': '', 'cside_gender': '', 'reversible': False, 'bf_role': 'end'}}
+    _DATABASE = {
+        'Samsung_NX1': {
+            'brand': 'Samsung',
+            'name': 'NX1',
+            'type': 'type_dslr',
+            'optical_length': 25.5,
+            'mass': 550,
+            'tside_thread': 'M42',
+            'tside_gender': 'Female',
+            'cside_thread': '',
+            'cside_gender': '',
+            'reversible': False,
+            'bf_role': 'end',
+            'sensor_width_mm': 23.5,
+            'sensor_height_mm': 15.7,
+            'width': 6480,
+            'height': 4320,
+            'pixel_size_um': 3.63
+        },
+        'Samsung_NX500': {
+            'brand': 'Samsung',
+            'name': 'NX500',
+            'type': 'type_dslr',
+            'optical_length': 25.5,
+            'mass': 292,
+            'tside_thread': 'M42',
+            'tside_gender': 'Female',
+            'cside_thread': '',
+            'cside_gender': '',
+            'reversible': False,
+            'bf_role': 'end',
+            'sensor_width_mm': 23.5,
+            'sensor_height_mm': 15.7,
+            'width': 6480,
+            'height': 4320,
+            'pixel_size_um': 3.63
+        },
+        'Samsung_NX300': {
+            'brand': 'Samsung',
+            'name': 'NX300',
+            'type': 'type_dslr',
+            'optical_length': 25.5,
+            'mass': 284,
+            'tside_thread': 'M42',
+            'tside_gender': 'Female',
+            'cside_thread': '',
+            'cside_gender': '',
+            'reversible': False,
+            'bf_role': 'end',
+            'sensor_width_mm': 23.5,
+            'sensor_height_mm': 15.7,
+            'width': 5472,
+            'height': 3648,
+            'pixel_size_um': 4.3
+        },
+        'Samsung_NX3000': {
+            'brand': 'Samsung',
+            'name': 'NX3000',
+            'type': 'type_dslr',
+            'optical_length': 25.5,
+            'mass': 250,
+            'tside_thread': 'M42',
+            'tside_gender': 'Female',
+            'cside_thread': '',
+            'cside_gender': '',
+            'reversible': False,
+            'bf_role': 'end',
+            'sensor_width_mm': 23.5,
+            'sensor_height_mm': 15.7,
+            'width': 5472,
+            'height': 3648,
+            'pixel_size_um': 4.3
+        }
+    }
 
     @classmethod
     def Samsung_NX1(cls):
