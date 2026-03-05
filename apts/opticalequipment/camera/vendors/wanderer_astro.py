f73d3c6665d90915cd058a7669db3e8d0ec07995
Łukasz Pożarlik <lpozarlik@gmail.com>
2026-03-05 15:46:59 +0100

    Add sensor specs to Wanderer Astro camera

--- apts/opticalequipment/camera/vendors/wanderer_astro.py
+++ apts/opticalequipment/camera/vendors/wanderer_astro.py
@@ -1,8 +1,31 @@
 from ..base import Camera
 
+
 class Wanderer_astroCamera(Camera):
-    _DATABASE = {'Wanderer_Astro_WanderCam_585C': {'brand': 'Wanderer Astro', 'name': 'WanderCam 585C', 'type': 'type_camera', 'optical_length': 6.5, 'mass': 160, 'tside_thread': 'M42', 'tside_gender': 'Female', 'cside_thread': '', 'cside_gender': '', 'reversible': False, 'bf_role': 'end'}}
+    _DATABASE = {
+        "Wanderer_Astro_WanderCam_585C": {
+            "brand": "Wanderer Astro",
+            "name": "WanderCam 585C",
+            "type": "type_camera",
+            "optical_length": 6.5,
+            "mass": 160,
+            "tside_thread": "M42",
+            "tside_gender": "Female",
+            "cside_thread": "",
+            "cside_gender": "",
+            "reversible": False,
+            "bf_role": "end",
+            "sensor_width_mm": 11.13,
+            "sensor_height_mm": 6.26,
+            "width": 3840,
+            "height": 2160,
+            "pixel_size_um": 2.9,
+            "quantum_efficiency_pct": 91,
+            "read_noise_e": 0.6,
+            "full_well_e": 40000,
+        }
+    }
 
     @classmethod
     def Wanderer_Astro_WanderCam_585C(cls):
-        return cls.from_database(cls._DATABASE['Wanderer_Astro_WanderCam_585C'])
+        return cls.from_database(cls._DATABASE["Wanderer_Astro_WanderCam_585C"])
