246e4126e2b12561abede657e4dfff19ed0b8e2c
Łukasz Pożarlik <lpozarlik@gmail.com>
2026-05-23 20:44:25 +0200

    Remove unused import in measure_units benchmark

--- examples/benchmarks/measure_units.py
+++ examples/benchmarks/measure_units.py
@@ -6,6 +6,5 @@ import os
 sys.path.insert(0, os.getcwd())
 
 start = time.time()
-from apts.units import ureg
 end = time.time()
 print(f"Importing apts.units and accessing ureg took: {end - start:.4f}s")
